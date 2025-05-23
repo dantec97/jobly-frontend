eedf77f7e1a7fe1f92ba3e808e021628ede0ef08
dantec97 <dantecpriority@gmail.com>
2025-05-23 13:33:25 -0400

    idk if removing a / will fix it but worth a shot

--- src/API/JoblyAPI.jsx
+++ src/API/JoblyAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://jobly-backend-e00a.onrender.com/";
+const BASE_URL = "https://jobly-backend-e00a.onrender.com";
 
 class JoblyAPI {
   static token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0.FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
