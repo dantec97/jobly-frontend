abf29eabb29f220c9fc9d04811df67986e67c3a2
dantec97 <dantecpriority@gmail.com>
2025-05-23 14:29:01 -0400

    we will not have default link styles...

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 function CompanyCard({ handle, name, description, logoUrl }) {
   return (
     <div className="CompanyCard">
+      <h3>
       <Link to={`/companies/${handle}`}>{name}</Link>
+      </h3>
       {logoUrl && <img src={logoUrl} alt={`${name} logo`} width={80} />}
       <p>{description}</p>
     </div>
